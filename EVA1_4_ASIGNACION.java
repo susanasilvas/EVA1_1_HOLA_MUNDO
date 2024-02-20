3a0f6099486164a33251ff3413895c5b8ba31adf
susanasilvas <159494432+susanasilvas@users.noreply.github.com>
2024-02-19 21:50:56 -0600

    Add files via upload

---
+++ EVA1_4_ASIGNACION.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_.asignacion;
+
+/**
+ *
+ * @author user
+ */
+public class EVA1_4_ASIGNACION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int edad;// DECLARACIÓN
+        edad = 20; // ASIGNACIÓN -- UN SOLO = SIGNIFICA ASIGNAR
+        System.out.println("Tu edad es");
+        System.out.println(edad);
+        
+        String nombre; //DECLACRACIÓN
+        nombre = "Susana Silva Santacruz";
+        System.out.println("Tu nombre es");
+        System.out.println(nombre);
+        
+        String carrera; // DECLARACIÓN
+        carrera = "Ingenieria en sistemas computacionales";
+        System.out.println("Tu carera es");
+        System.out.println(carrera);
+    }
+    
+}
